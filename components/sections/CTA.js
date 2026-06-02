001e48c33e833abf15a65cc0070fb1678fb604b0
panjisap0805 <panjisap0805@gmail.com>
2026-06-02 15:00:20 +0700

    button CTA width

--- components/sections/CTA.js
+++ components/sections/CTA.js
@@ -15,7 +15,7 @@ export default function CTA({ isProject, nextLink, nextProjectName }) {
           <span className="text-[14vw] md:text-7xl lg:text-[128px] text-primary">TOUCH!</span>
         </h2>
 
-        <div className="flex flex-col sm:flex-row gap-6 items-center">
+        <div className="flex flex-col sm:flex-row gap-6 items-stretch sm:items-center w-full sm:w-auto">
           <a href="https://www.linkedin.com/in/panjisap0805/" target="_blank" rel="noopener noreferrer" className="group bg-white text-black font-bold text-lg px-8 py-4 uppercase tracking-wider text-center brutalist-border brutalist-shadow-primary hover:translate-y-[2px] hover:translate-x-[2px] hover:shadow-[2px_2px_0_0_#0f49bd] transition-all flex w-full sm:w-auto justify-center items-center gap-2">
             [LINKEDIN]
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
